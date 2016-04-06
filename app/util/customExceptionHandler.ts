19513448f208ea2cab0a250262861718d9d9cb62
dylanvdmerwe <dylanvdmerwe@gmail.com>
2016-04-06 22:38:05 +0200

    enable tracekit

--- app/util/customExceptionHandler.ts
+++ app/util/customExceptionHandler.ts
@@ -1,11 +1,12 @@
 import {Injectable} from 'angular2/core';
+import * as TraceKit from 'tracekit'
 
 @Injectable()
 export class CustomExceptionHandler {
-  call(error, stackTrace = null, reason = null) {
-      console.log('custom exception handler...');
-    console.error(error, [stackTrace, reason]);
-    
-   // TraceKit.report(e); //error with stack trace gets normalized and sent to subscriber
-  }
+    call(error, stackTrace = null, reason = null) {
+        console.log('custom exception handler...');
+        console.error(error, [stackTrace, reason]);
+
+        TraceKit.report(error); //error with stack trace gets normalized and sent to subscriber
+    }
 }
--- app/util/errorReporter.ts
+++ app/util/errorReporter.ts
@@ -1,13 +1,16 @@
 import {Injectable} from 'angular2/core';
+import * as TraceKit from 'tracekit'
 
 @Injectable()
 export class ErrorReporter {
-    init(){
-       // TraceKit.report.subscribe(this.reportError);
-       console.log("ErrorReporter is initialized");
+    init() {
+        TraceKit.report.subscribe(this.reportError);
+        console.log("ErrorReporter is initialized");
     }
-    
-    reportError(error){
+
+    reportError(error) {
         // do something with the error
+        console.log("error subscription fired");
+        console.log(error);
     }
 }
